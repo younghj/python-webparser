1fa724ce4f3304798e9c1db018bf956642636d55
Tommy Jung <jung4351@gmail.com>
2014-06-26 22:52:34 -0400

    second time deletion

--- all.py
+++
@@ -1,5 +0,0 @@
-import os
-#runs these 3 programs using python if they are in the same directory
-os.system('python initialize.py')
-os.system('python extracthttp.py')
-os.system('python extracthttp2.py')
